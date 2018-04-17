5d97662e850bfc0cc987ca92bd41a5019891c4a5
Jared Raynes <ray07c@csiro.au>
2018-04-17 14:35:45 +1000

    saliva sheet

---
+++ R_scripts/saliva.r
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(lmerTest)
+library(emmeans)
+
+Saliva <- read_xlsx("Data/Copy of 20180413_DataSchool_Glycogen.xlsx", sheet = "Saliva")
+
+#dont need to do anything with this data set as it is just for 
+#
