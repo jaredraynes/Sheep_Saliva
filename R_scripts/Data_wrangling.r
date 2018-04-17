79608ed6ba333b2eda386cc0a740be1b0923ad0a
Jared Raynes <ray07c@csiro.au>
2018-04-17 13:57:05 +1000

    update

--- R_scripts/Data_wrangling.r
+++ R_scripts/Data_wrangling.r
@@ -59,18 +59,18 @@ ggplot(Cortisol_long, aes(x = Treatment.Group, y = `Cortisol_nmol/L`, colour = T
 ### Glycogen data wrangling
 Glycogen <- Glycogen[-c(31),] ## remove error message
 
-G1 <- Glycogen[1:5]
-G2 <- Glycogen[6:10]
+G1 <- Glycogen[1:5] #the treatment groups are in different columns this time
+G2 <- Glycogen[6:10] #need to have identical names to use row_bind
 names(G2) <- c("Treatment group", "Brand", "Glycogen mg/g tissue", "Lactate mg/g", "Corrected glycogen")
-G2$`Treatment group` <- as.character(G2$`Treatment group`)
-Glycogen_comb <- bind_rows(G1, G2)
+G2$`Treatment group` <- as.character(G2$`Treatment group`) 
+Glycogen_comb <- bind_rows(G1, G2) #combining the columns together
 Glycogen_long <- Glycogen_comb %>% 
   gather(key = "Measure", value = "mg/g", "Glycogen mg/g tissue", "Lactate mg/g", "Corrected glycogen")
 names(Glycogen_long) <- c("Treatment.Group", "ID", "Measure", "mg/g")
 Glycogen_long$Treatment.Group <- factor(Glycogen_long$Treatment.Group)
 Glycogen_long$ID <- factor(Glycogen_long$ID)
 
-#Cortisol initial look
+#Glycogen initial look
 
 ggplot(Glycogen_long, aes(x = Treatment.Group, y = `mg/g`, colour = Measure))+
   geom_boxplot() 
@@ -78,4 +78,42 @@ ggplot(Glycogen_long, aes(x = Treatment.Group, y = `mg/g`, colour = Measure))+
 ##looks like stress (treatments 2 and 4) causes lower amount of glycogen, feed
 # maybe has an influence
 
+### Glycogen_biopsy data wrangling
+
+Glycogen_biopsy <- Glycogen_biopsy[1:4]
+Glycogen_biopsy <- Glycogen_biopsy[-c(12:14),]
+
+Glycogen_biopsy$Treatment.Group <- c(1,1,1,1,1,2,2,2,2,2,2)
+Glycogen_biopsy_long <- Glycogen_biopsy %>% 
+  gather(key = "Measure", value = "mg/g", "Glycogen mg/g", "Lactate mg/g", "Corrected Glycogen")
+names(Glycogen_biopsy_long) <- c("ID", "Treatment.Group", "Measure", "mg/g")
+Glycogen_biopsy_long$Treatment.Group <- factor(Glycogen_biopsy_long$Treatment.Group)
+Glycogen_biopsy_long$ID <- factor(Glycogen_biopsy_long$ID)
+
+#Glycogen_ Biopsy initial look
+
+ggplot(Glycogen_biopsy_long, aes(x = Treatment.Group, y = `mg/g`, colour = Measure))+
+  geom_boxplot() 
+
+##stress causes lower amount of glycogen 
+
+
+###Weight Gain data wrangling
+
+Weight_gain <- Weight_gain[1:8]
+Weight_gain <- Weight_gain[-c(61),]
+names(Weight_gain) <- c("Treatment.Group", "ID", "-1", "0", "1", "2", "3", "4")
+Weight_gain_long <- Weight_gain %>% 
+  gather(key = "Week", value = "Weight.kg", "-1", "0", "1", "2", "3", "4")
+Weight_gain_long$Treatment.Group <- factor(Weight_gain_long$Treatment.Group)
+Weight_gain_long$ID <- factor(Weight_gain_long$ID)
+Weight_gain_long$Week <- factor(Weight_gain_long$Week)
+
+##initial look at weight gain
+
+ggplot(Weight_gain_long, aes(x = Week, y = Weight.kg, colour = Treatment.Group))+
+  geom_boxplot()
+
+## treatments no difference at birth, restricted feed lowers weight,
+## stress lowers weight
 
