fb3df7f6ad6a8abc33823369864621ffd04632b2
Jared Raynes <ray07c@csiro.au>
2018-04-17 12:32:11 +1000

    Updated with more wrangling

--- R_scripts/Data_wrangling.r
+++ R_scripts/Data_wrangling.r
@@ -24,4 +24,58 @@ names(pH) <- c("Treatment.Group", "ID", "M.semitendinosus", "M.longissimus.dorsi
 pH_long <- pH %>% 
   gather(key = "Muscle.type", value = "pH", "M.semitendinosus", "M.longissimus.dorsi")
 
+pH_long$Treatment.Group <- factor(pH$Treatment.Group)
+pH_long$ID <- factor(pH$ID)    
+
+#pH initial look
+
+ggplot(pH_long, aes(x = Treatment.Group, y = pH, colour = Muscle.type))+
+  geom_boxplot()
+
+######### looks like there is definitely an effect of stress on the pH
+#### of both muscle types. M. Semit also consistantly higher pH. no effect
+### on feeding
+
+#Cortisol wrangling
+
+Cortisol <- Cortisol[1:5] #removing all columns that aren't needed
+names(Cortisol) <- c("Treatment.Group", "ID", "1", "2", "3")
+Cortisol_long <- Cortisol %>% 
+  gather(key = "Time.point", value = "Cortisol_nmol/L", "1", "2", "3")
+Cortisol_long$Treatment.Group <- factor(Cortisol_long$Treatment.Group)
+Cortisol_long$ID <- factor(Cortisol_long$ID)
+Cortisol_long$Time.point <- factor(Cortisol_long$Time.point)
+
+#Cortisol initial look
+
+ggplot(Cortisol_long, aes(x = Treatment.Group, y = `Cortisol_nmol/L`, colour = Time.point))+
+  geom_boxplot() +
+  scale_y_log10()
+
+##highest cortisol after kill(time.point3), drop in cortisol after stress
+##looks like no influence for treatment group (maybe combine)
+
+
+### Glycogen data wrangling
+Glycogen <- Glycogen[-c(31),] ## remove error message
+
+G1 <- Glycogen[1:5]
+G2 <- Glycogen[6:10]
+names(G2) <- c("Treatment group", "Brand", "Glycogen mg/g tissue", "Lactate mg/g", "Corrected glycogen")
+G2$`Treatment group` <- as.character(G2$`Treatment group`)
+Glycogen_comb <- bind_rows(G1, G2)
+Glycogen_long <- Glycogen_comb %>% 
+  gather(key = "Measure", value = "mg/g", "Glycogen mg/g tissue", "Lactate mg/g", "Corrected glycogen")
+names(Glycogen_long) <- c("Treatment.Group", "ID", "Measure", "mg/g")
+Glycogen_long$Treatment.Group <- factor(Glycogen_long$Treatment.Group)
+Glycogen_long$ID <- factor(Glycogen_long$ID)
+
+#Cortisol initial look
+
+ggplot(Glycogen_long, aes(x = Treatment.Group, y = `mg/g`, colour = Measure))+
+  geom_boxplot() 
+
+##looks like stress (treatments 2 and 4) causes lower amount of glycogen, feed
+# maybe has an influence
+
 
